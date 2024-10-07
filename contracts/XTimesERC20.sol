5e859cd87ad1c4cfdf730e78ce423b8f6882026b
Carlos Silva <carlos_silva071@hotmail.com>
2024-10-07 11:03:55 -0300

    Changes `XTimesERC20` to implements `ERC20Capped` logic to ensure safety.

--- contracts/XTimesERC20.sol
+++ contracts/XTimesERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
@@ -11,20 +11,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @custom:site https://docs.cointimes.network/
  */
 
-contract XTimesERC20 is ERC20, Ownable {
-    uint256 private MAX_TOKEN_SUPPLY = 1_000_000_000 * 10 ** decimals();
+contract XTimesERC20 is ERC20Capped, Ownable {
+    /** @dev XTimes cap so that never exists more than 1 billion tokens at a time */
+    uint32 public constant MAX_TOKEN_SUPPLY = 1_000_000_000;
 
     constructor()
         ERC20("X-Times", "XTIMES")
-        Ownable(msg.sender) {}
+        ERC20Capped(MAX_TOKEN_SUPPLY * 10 ** decimals())
+        Ownable(msg.sender)
+        {}
 
     function maxSupply() public view returns (uint256)  {
-        return MAX_TOKEN_SUPPLY;
+        return cap();
     }
 
     function mint(address account, uint256 value) external onlyOwner {
         require(value > 0, "Invalid amount");
-        require(totalSupply() + value <= MAX_TOKEN_SUPPLY, "Maximum token supply reached!");
         _mint(account, value);
     }
 }
